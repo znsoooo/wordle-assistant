41c82a7d6167310def2b4325a99775c5bd38b3a9
lsx <11313213@qq.com>
2022-12-28 21:55:23 +0800

    Feat: 忽略大小写、按`Esc`重置、增加窗口滚动条、更改窗口文本

--- wordle.py
+++ wordle.py
@@ -1,18 +1,24 @@
 import re
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+
+def clear(*v):
+    vars[0].set('*****')
+    vars[1].set('')
+    vars[2].set('')
 
 def refresh(*v):
-    s1 = vars[0].get()
-    s2 = vars[1].get()
-    s3 = vars[2].get()
+    s1 = vars[0].get().lower()
+    s2 = vars[1].get().lower()
+    s3 = vars[2].get().lower()
 
     for c in ' _*?':
-        s1 = s1.replace(c, '.')
-    patt = re.compile(s1)
+        s1 = s1.replace(c, '[a-z]')
+    patt = re.compile(s1, re.I)
 
     results = [w for w in words
-               if  all(c     in w for c in s2)
-               and all(c not in w for c in s3)
+               if  all(c     in w.lower() for c in s2)
+               and all(c not in w.lower() for c in s3)
                and patt.fullmatch(w)]
 
     txt.delete('1.0', 'end')
@@ -26,15 +32,16 @@ top = tk.Tk()
 top.title('Wordle Assistant')
 
 vars = []
-for i, name in enumerate(['Green', 'Yellow', 'Gray']):
+for i, name in enumerate(['Pattern', 'Include', 'Exclude']):
     var = tk.StringVar()
     var.trace('w', refresh)
     vars.append(var)
     tk.Label(top, text=name+':').grid(row=0, column=i*2)
     tk.Entry(top, textvariable=var).grid(row=0, column=i*2+1)
 
-txt = tk.Text(top)
+txt = ScrolledText(top)
 txt.grid(row=3, column=0, columnspan=6)
+top.bind('<Escape>', clear)
 
 vars[0].set('*****')
 
